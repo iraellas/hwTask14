330834df2467848c9b63c027749a4ff38b4a5081
Irina Detochenko <irina.detochenko@gmail.com>
2021-04-30 18:15:08 +0300

    some changes

--- src/main/java/ProductP.java
+++ src/main/java/ProductP.java
@@ -2,6 +2,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ProductP {
-    @FindBy(css = "div.a-row>h1")
-    private WebElement infoMessage;
+    @FindBy(id = "add-to-cart-button")
+    private WebElement buttonAddToCart;
 }
